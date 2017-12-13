486a01cb3d64238b938aa4d0be6aeaf0aeffc338
Shawn Renner <resume@shawnrenner.com>
2017-12-13 16:07:04 -0600

    api key works

---
+++ StockConsole/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+
+namespace StockConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string apiKey = ConfigurationManager.AppSettings["apiKey"].ToString();
+            Console.WriteLine("Using API key " + apiKey);
+
+
+
+            string html = GetTimeSeriesIntraday("F");
+
+            Console.WriteLine(html);
+
+            Console.ReadKey();
+        }
+
+        private static string GetTimeSeriesIntraday(string symbol)
+        {
+            string testURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=1min&apikey=" + ConfigurationManager.AppSettings["apiKey"].ToString();
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(testURL);
+            string html = "";
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        html = reader.ReadToEnd();
+                    }
+                }
+            }
+
+            return html;
+        }
+
+    }
+}
