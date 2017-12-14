1d25823c46f3733938076f1ddbbdfbf87c792523
Shawn Renner <resume@shawnrenner.com>
2017-12-14 14:48:42 -0600

    isactive flag (in case fetcher does not have symbol data)

--- StockLibrary/Fund.cs
+++ StockLibrary/Fund.cs
@@ -9,5 +9,6 @@ namespace StockLibrary
         public string Symbol { get; set; }
         public string Name { get; set; }
         public string Exchange { get; set; }
+        public bool IsActive { get; set; }
     }
 }
