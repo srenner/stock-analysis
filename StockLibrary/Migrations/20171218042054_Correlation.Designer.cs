07715ec30c3126c199cf02337c1ffbf07835f2ec
Shawn Renner <resume@shawnrenner.com>
2017-12-17 22:22:08 -0600

    update database

--- StockLibrary/Migrations/20171218042054_Correlation.Designer.cs
+++ StockLibrary/Migrations/20171218042054_Correlation.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace StockLibrary.Migrations
 {
     [DbContext(typeof(SqliteContext))]
-    partial class SqliteContextModelSnapshot : ModelSnapshot
+    [Migration("20171218042054_Correlation")]
+    partial class Correlation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ StockLibrary/Migrations/20171218042054_Correlation.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace StockLibrary.Migrations
+{
+    public partial class Correlation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CorrelatedIncrease",
+                columns: table => new
+                {
+                    CorrelatedIncreaseID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PrimaryFundDayID = table.Column<int>(nullable: false),
+                    SecondaryFundDayID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CorrelatedIncrease", x => x.CorrelatedIncreaseID);
+                    table.ForeignKey(
+                        name: "FK_CorrelatedIncrease_FundDay_PrimaryFundDayID",
+                        column: x => x.PrimaryFundDayID,
+                        principalTable: "FundDay",
+                        principalColumn: "FundDayID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CorrelatedIncrease_FundDay_SecondaryFundDayID",
+                        column: x => x.SecondaryFundDayID,
+                        principalTable: "FundDay",
+                        principalColumn: "FundDayID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CorrelatedIncrease_PrimaryFundDayID",
+                table: "CorrelatedIncrease",
+                column: "PrimaryFundDayID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CorrelatedIncrease_SecondaryFundDayID",
+                table: "CorrelatedIncrease",
+                column: "SecondaryFundDayID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CorrelatedIncrease");
+        }
+    }
+}
