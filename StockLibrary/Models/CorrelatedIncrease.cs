d59fb5b9a65f2b1d5be45a67c9dd0e065f734981
Shawn Renner <resume@shawnrenner.com>
2017-12-17 22:19:44 -0600

    start correlation model

---
+++ StockLibrary/Models/CorrelatedIncrease.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockLibrary.Models
+{
+    public class CorrelatedIncrease
+    {
+        public int CorrelatedIncreaseID { get; set; }
+
+        public int PrimaryFundDayID { get; set; }
+        public FundDay PrimaryFundDay { get; set; }
+
+        public int SecondaryFundDayID { get; set; }
+        public FundDay SecondaryFundDay { get; set; }
+
+    }
+}
